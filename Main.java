b5b7d1356d0fd8960e142251208abcae63587d6c
faust <faustmiller@gmail.com>
2021-12-02 12:43:25 +0500

    onecommit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientBalanceMoney = 2_000_000_000;
+        int moneyTransfer = 500_000_000;
+        int totalSum;
+        totalSum = moneyTransfer + clientBalanceMoney;
+        System.out.println(totalSum);
+    }
+}
